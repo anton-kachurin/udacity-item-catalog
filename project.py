d5e6249f76d58bc3d6751f3e12fbfaa07058ca19
Anton Kachurin <kachurinanton@gmail.com>
2016-12-23 23:29:59 -0500

    define routes

--- project.py
+++ project.py
@@ -11,6 +11,25 @@ def redirect_to_main():
 def show_catalog():
     return 'catalog page with a list of categories'
 
+@app.route('/catalog/<string:category_name>')
+def show_category(category_name):
+    return 'some category selected with name ' + category_name
+
+@app.route('/catalog/<string:category_name>/add')
+def add_item(category_name):
+    return 'item creation page'
+
+@app.route('/catalog/<string:category_name>/<string:item_name>')
+def show_item(category_name, item_name):
+    return 'show some item on the page'
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit')
+def edit_item(category_name, item_name):
+    return 'edit some item'
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete')
+def delete_item(category_name, item_name):
+    return 'delete some item'
 
 
 if __name__ == '__main__':
